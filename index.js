7cac66a57e7f1f5778071f73cb3d01d3c4cd0d42
Jennifer Charney <jenfromzen@yahoo.com>
2018-12-25 05:59:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 var a, b
 
-beforeEach(function() {
   a = Math.floor(Math.random() * 1000)
   b = Math.floor(Math.random() * 1000)
-})
 
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+
+
+
+
+it('divide(a, b) divides a by b and returns the result', function() {
+  expect(divide(a, b)).toEqual(a / b)
+})
